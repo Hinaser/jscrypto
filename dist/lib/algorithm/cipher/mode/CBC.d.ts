7596139d439e018f6841812d20feee799aa060f8
Hinaser <rindo.hinase@gmail.com>
2020-09-23 00:59:29 +0900

    Fixed CBC's inner class declaration error

--- dist/lib/algorithm/cipher/mode/CBC.d.ts
+++ dist/lib/algorithm/cipher/mode/CBC.d.ts
@@ -1,46 +1,30 @@
 import { BlockCipherMode, BlockCipherModeProps } from "./BlockCipherMode";
 export interface CBCProps extends BlockCipherModeProps {
 }
-declare class CBCEncrypter extends BlockCipherMode {
-    protected _prevBlock: number[];
-    constructor(props: CBCProps);
-    /**
-     * Processes the data block at offset.
-     *
-     * @param {number[]} words The data words to operate on.
-     * @param {number} offset The offset where the block starts.
-     * @example
-     *   mode.processBlock(data.words, offset);
-     */
-    processBlock(words: number[], offset: number): void;
-}
-declare class CBCDecrypter extends BlockCipherMode {
-    protected _prevBlock: number[];
-    constructor(props: CBCProps);
-    /**
-     * Processes the data block at offset.
-     *
-     * @param {number[]} words The data words to operate on.
-     * @param {number} offset The offset where the block starts.
-     * @example
-     *   mode.processBlock(data.words, offset);
-     */
-    processBlock(words: number[], offset: number): void;
-}
 export declare class CBC extends BlockCipherMode {
+    protected _prevBlock: number[];
+    /**
+     * CBC encryptor.
+     */
+    static Encrypter: typeof CBC;
+    /**
+     * CBC decryptor.
+     */
+    static Decrypter: typeof CBC;
+    constructor(props: CBCProps);
+    xorBlock(words: number[], offset: number, blockSize: number): void;
     /**
      * Creates this mode for encryption.
      * @param {BlockCipherModeProps} props
      * @example
      *   var mode = JsCrypto.CBC.createEncryptor(cipher, iv.words);
      */
-    static createEncrypter(props: CBCProps): CBCEncrypter;
+    static createEncrypter(props: CBCProps): CBC;
     /**
      * Creates this mode for decryption.
      * @param {BlockCipherModeProps} props
      * @example
      *   var mode = JsCrypto.CBC.createDecryptor(cipher, iv.words);
      */
-    static createDecrypter(props: CBCProps): CBCDecrypter;
+    static createDecrypter(props: CBCProps): CBC;
 }
-export {};

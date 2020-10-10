8b73fcbffd323d79c89fa2dea8b6a760397eab0f
Hinaser <rindo.hinase@gmail.com>
2020-10-10 21:38:07 +0900

    Created tests for Hex/Latin1/Utf8

---
+++ test/specs/encoder/Utf8.js
@@ -0,0 +1,47 @@
+const expect = require("expect.js");
+const {
+  Utf8,
+  Word32Array,
+} = require("../../../test/build/all");
+
+describe("encoder/Latin1", function(){
+  it("test stringify 1", function(){
+    const expectedValue = "$";
+    expect(Utf8.stringify(new Word32Array([0x24000000], 1))).to.be(expectedValue);
+  });
+  
+  it("test stringify 2", function(){
+    const expectedValue = "¢";
+    expect(Utf8.stringify(new Word32Array([0xc2a20000], 2))).to.be(expectedValue);
+  });
+  
+  it("test stringify 3", function(){
+    const expectedValue = "€";
+    expect(Utf8.stringify(new Word32Array([0xe282ac00], 3))).to.be(expectedValue);
+  });
+  
+  it("test stringify 4", function(){
+    const expectedValue = "𤭢";
+    expect(Utf8.stringify(new Word32Array([0xf0a4ada2], 4))).to.be(expectedValue);
+  });
+  
+  it("test parse 1", function(){
+    const expectedValue = new Word32Array([0x24000000], 1).toString();
+    expect(Utf8.parse("$").toString()).to.be(expectedValue);
+  });
+  
+  it("test parse 2", function(){
+    const expectedValue = new Word32Array([0xc2a20000], 2).toString();
+    expect(Utf8.parse("¢").toString()).to.be(expectedValue);
+  });
+  
+  it("test parse 3", function(){
+    const expectedValue = new Word32Array([0xe282ac00], 3).toString();
+    expect(Utf8.parse("€").toString()).to.be(expectedValue);
+  });
+  
+  it("test parse 4", function(){
+    const expectedValue = new Word32Array([0xf0a4ada2], 4).toString();
+    expect(Utf8.parse("𤭢").toString()).to.be(expectedValue);
+  });
+});
